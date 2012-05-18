88275cc23b9cfd9c903c2db29aacec1f0f3cdd97
nenTi <tobinenti@gmail.com>
2012-05-19 01:23:20 +0200

    fix config error

--- server_config_example.py
+++ server_config_example.py
@@ -6,6 +6,6 @@ site_config = {
 
 
 db_config = {
-     "AWS_ACCESS_KEY_ID": "AKIAI35NZVVHQ5244MMA", 
-     "AWS_SECRET_ACCESS_KEY": "JDMqNSFeDGII8hwTinDon65POFcGZGetknOvZ5H+"
+     "AWS_ACCESS_KEY_ID": "", 
+     "AWS_SECRET_ACCESS_KEY": ""
 }
