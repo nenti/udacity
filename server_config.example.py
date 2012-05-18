433eb518a633a12c28ff910e2859f662919314ce
nenTi <tobinenti@gmail.com>
2012-05-19 01:15:48 +0200

    fixed security error

---
+++ server_config.example.py
@@ -0,0 +1,11 @@
+site_config = {
+    'site_title' : 'ScotlandOelde', 
+    'google_analytics' : '', 
+    'feed_url' : '', 
+}
+
+
+db_config = {
+     "AWS_ACCESS_KEY_ID": "AKIAI35NZVVHQ5244MMA", 
+     "AWS_SECRET_ACCESS_KEY": "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"
+}
